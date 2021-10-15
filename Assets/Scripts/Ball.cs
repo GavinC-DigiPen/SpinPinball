6fb52b97f9c30ecbd6d7f677c9fb828613461410
GavinCoupDigipen <53310510+GavinCoupDigipen@users.noreply.github.com>
2021-10-14 18:05:30 -0700

    Create Paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿//------------------------------------------------------------------------------
+//
+// File Name:	Ball.cs
+// Author(s):	Gavin Cooper (gavin.cooper@digipen.edu)
+// Project:	    SpinPinball
+// Course:	    WANIC VGP2
+//
+// Copyright © 2021 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [Tooltip("Starting velocity of the ball")]
+    public float startingVelocity = 8;
+    [Tooltip("Max velocity")]
+    public float maxVelocity = 10;
+    [Tooltip("Button that will reset the ball to 0 0")]
+    public KeyCode resetBall = KeyCode.Space;
+
+    private Rigidbody2D ballRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float rotation = Random.Range(0, 24) * 15;
+        transform.Rotate(0, 0, rotation, Space.Self);
+        ballRB = GetComponent<Rigidbody2D>();
+        ballRB.velocity = transform.up * startingVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(resetBall))
+        {
+            transform.position = new Vector2(0, 0);
+        }
+
+        ballRB.velocity = Vector3.ClampMagnitude(ballRB.velocity, maxVelocity);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,77 @@
+﻿//------------------------------------------------------------------------------
+//
+// File Name:	Paddle.cs
+// Author(s):	Gavin Cooper (gavin.cooper@digipen.edu)
+// Project:	    SpinPinball
+// Course:	    WANIC VGP2
+//
+// Copyright © 2021 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [Tooltip("Key to spin right")]
+    public KeyCode spinRightKey = KeyCode.D;
+    [Tooltip("Key to spin left")]
+    public KeyCode spinLeftKey = KeyCode.A;
+    [Tooltip("The speed the paddle will rotate")]
+    public float rotationSpeed = 5;
+    [Tooltip("The speed the paddle will go around the circle")]
+    public Vector2 circleSpeed = new Vector2(5, 5);
+    [Tooltip("The radius of the circle the paddle will go around")]
+    public Vector2 circleRadius = new Vector2(1, 1);
+
+    private Vector2 startingLocation;
+    private int dirrection = 0;
+    private float mathTimer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingLocation = transform.position;
+    }
+
+    // Update is called at a fixed rate
+    private void FixedUpdate()
+    {
+        // Get dirrection
+        if (Input.GetKeyDown(spinLeftKey))
+        {
+            dirrection = -1;
+        }
+        else if (Input.GetKey(spinLeftKey) && !Input.GetKey(spinRightKey))
+        {
+            dirrection = -1;
+        }
+        else if (Input.GetKeyDown(spinRightKey))
+        {
+            dirrection = 1;
+        }
+        else if (Input.GetKey(spinRightKey) && !Input.GetKey(spinLeftKey))
+        {
+            dirrection = 1;
+        }
+        else if (!Input.GetKey(spinLeftKey) && !Input.GetKey(spinRightKey))
+        {
+            dirrection = 0;
+        }
+
+        // Movement 
+        transform.Rotate(0, 0, -rotationSpeed * dirrection, Space.Self);
+
+        if (dirrection == 1)
+        {
+            mathTimer += 0.01f;
+        }
+        else if (dirrection == -1)
+        {
+            mathTimer -= 0.01f;
+        }
+        transform.position = new Vector2(circleRadius.x * Mathf.Cos(mathTimer * circleSpeed.x) + startingLocation.x, circleRadius.y * Mathf.Sin(mathTimer * circleSpeed.y) + startingLocation.y);
+    }
+}
